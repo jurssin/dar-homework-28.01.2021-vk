86e734fdf599dd209663d0f95d3fca3b721b36a9
Syrym Zhursin <jurssin@gmail.com>
2021-01-25 18:11:59 +0600

    Initial Commit

---
+++ Lesson - 4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson - 4
+//
+//  Created by User on 1/25/21.
+//  Copyright © 2021 Syrym Zhursin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
